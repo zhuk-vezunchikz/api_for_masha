0ac4bbf8948204bd1ce2ca17c97c3be56cb0be9e
zhuk <evgeniy.zhuk.by@gmail.com>
2022-03-25 20:24:28 +0300

    1

--- src/controllers/profile/getProfile.js
+++ src/controllers/profile/getProfile.js
@@ -4,10 +4,8 @@ const Profile = require('../../models/Profile.js')
 const getProfile = async (req, res) => {
     try {
         const profile = await Profile.find()
-        console.log(profile)
         return res.json(profile)
     } catch (e) {
-        console.log(e)
         res.status(500).json({message: 'Something happened in getProfile...'})
     }
 }
